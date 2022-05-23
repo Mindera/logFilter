ee4322dbd951369b190a2e28314303d775d2f4df
LuisFaria <57047301+luisfcfaria@users.noreply.github.com>
2022-05-23 15:39:39 +0100

    endpoints

---
+++ src/main/java/mindera/solverde/mockapi/persistence/controller/MockController.java
@@ -0,0 +1,28 @@
+package mindera.solverde.mockapi.persistence.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MockController {
+
+
+    @GetMapping("/get")
+    public ResponseEntity<String> getHello() {
+        String result = "hello get";
+
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<String> postMessage(@RequestBody String post){
+
+        return  ResponseEntity.ok(post);
+    }
+
+    @PutMapping("/put")
+    public ResponseEntity<String> putMessage(@RequestBody String put){
+
+        return ResponseEntity.ok(put);
+    }
+}
