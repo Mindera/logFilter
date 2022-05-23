bcd1d4ebd00ab5fed7c79dce92dd705ef7d8aed8
LuisFaria <57047301+luisfcfaria@users.noreply.github.com>
2022-05-23 15:05:54 +0100

    project setup

---
+++ src/main/java/mindera/solverde/mockapi/MockapiApplication.java
@@ -0,0 +1,13 @@
+package mindera.solverde.mockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockapiApplication.class, args);
+	}
+
+}
