c26e6dc4616ed270bfd5ec1b18ca26eedd68f994
DiogoNoronha <diogo.noronha@gmail.com>
2022-06-03 10:12:52 +0100

    V1 - Without Spring

--- src/main/java/mindera/solverde/logfilter/service/RequestResponseLoggingFilter.java
+++ src/main/java/mindera/solverde/logfilter/service/RequestResponseLoggingFilter.java
@@ -5,14 +5,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import mindera.solverde.logfilter.models.Log;
 import mindera.solverde.logfilter.models.Request;
 import mindera.solverde.logfilter.models.Response;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-import org.springframework.web.util.ContentCachingRequestWrapper;
-import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 import java.io.*;
 import java.util.Collections;
 import java.util.Map;
@@ -23,13 +20,14 @@ public class RequestResponseLoggingFilter implements Filter {
 
     private final ObjectMapper objectMapper;
 
+
     public RequestResponseLoggingFilter() {
         objectMapper = new ObjectMapper()
 //                .enable(SerializationFeature.INDENT_OUTPUT)
                 .configure(JsonGenerator.Feature.AUTO_CLOSE_TARGET, false);
     }
 
-    @Override
+/*    @Override
     public void doFilter(ServletRequest request,
                          ServletResponse response,
                          FilterChain filterChain) throws IOException, ServletException {
@@ -50,38 +48,65 @@ public class RequestResponseLoggingFilter implements Filter {
 
         generateLog(requestWrapper, responseWrapper, requestString, responseStr);
 
+    }*/
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        final ByteArrayPrintWriter pw = new ByteArrayPrintWriter(baos);
+
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+        String requestBody = getRequestBody(httpRequest);
+
+        HttpServletResponse wrappedResp = new HttpServletResponseWrapper(httpResponse) {
+            @Override
+            public PrintWriter getWriter() {
+                return pw;
+            }
+
+            @Override
+            public ServletOutputStream getOutputStream() {
+                return new ServletOutputStream() {
+                    @Override
+                    public boolean isReady() {
+                        return false;
+                    }
+
+                    @Override
+                    public void setWriteListener(WriteListener writeListener) {
+                    }
+
+                    @Override
+                    public void write(int b) {
+                        baos.write(b);
+                    }
+                };
+            }
+        };
+
+        filterChain.doFilter(servletRequest, wrappedResp);
+
+        byte[] bytes = baos.toByteArray();
+        String responseStr = new String(bytes);
+        servletResponse.getOutputStream().write(bytes);
+
+        generateLog(httpRequest, httpResponse, requestBody, responseStr);
     }
-//
-//    @Override
-//    public void doFilter(ServletRequest request,
-//                         ServletResponse response,
-//                         FilterChain filterChain) throws IOException, ServletException {
-//
-//        HttpServletRequest httpRequest = (HttpServletRequest) request;
-//        HttpServletResponse httpResponse = (HttpServletResponse) response;
-//
-//        String requestBody = getRequestBody(httpRequest);
-//        String responseBody = getResponseBody(httpResponse);
-//
-//        generateLog(httpRequest, httpResponse, requestBody, responseBody);
-//
-//        filterChain.doFilter(request, response);
-//    }
-//
-//    public String getRequestBody(HttpServletRequest httpRequest) throws IOException {
-//        BufferedReader reader = httpRequest.getReader();
-//        StringBuilder sb = new StringBuilder();
-//        String line;
-//        while ((line = reader.readLine()) != null) {
-//            sb.append(line);
-//        }
-//        return sb.toString();
-//    }
-//
-//    public String getResponseBody(HttpServletResponse httpResponse) throws IOException {
-//
-//        return "hello from response";
-//    }
+
+    public String getRequestBody(HttpServletRequest httpRequest) throws IOException {
+        BufferedReader reader = httpRequest.getReader();
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+        return sb.toString();
+    }
 
 
     public void generateLog(HttpServletRequest req, HttpServletResponse res, String requestString, String responseStr) throws IOException {
@@ -108,4 +133,9 @@ public class RequestResponseLoggingFilter implements Filter {
         objectMapper.writeValue(System.out, log);
     }
 
+    public static class ByteArrayPrintWriter extends PrintWriter {
+        public ByteArrayPrintWriter(OutputStream out) {
+            super(out);
+        }
+    }
 }
